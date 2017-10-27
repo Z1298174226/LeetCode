f5711587e98ddd51576ddf6c7336c6d7df2f88bf
Z1298174226 <1298174226@qq.com>
2017-10-27 08:38:30 +0800

    first commit

---
+++ src/leetcode/ConcatenatedWords.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * Created by qtfs on 2017/10/27.
+ */
+public class ConcatenatedWords {
+    public static List<String> findAllConcatenatedWordsInADict(String[] words) {
+        List<String> list = new ArrayList<String>();
+        Map<String, Set<String>> map = new HashMap<String, Set<String>>();
+        for(String word : words) {
+            if(dfs(word, words, map).size() > 1)
+                list.add(word);
+        }
+        return list;
+    }
+    private static Set<String> dfs(String s, String[] wordList, Map<String, Set<String>> map) {
+        if(map.containsValue(s)) {
+            return map.get(s);
+        }
+        Set<String> list = new HashSet<String>();
+        if(s.length() == 0)
+            return list;
+        for(String word : wordList) {
+            if(word != s) {
+                if (s.startsWith(word)) {
+                    list.addAll(dfs(s.substring(word.length()), wordList, map));
+                    list.add(word);
+                }
+            }
+        }
+        map.put(s, list);
+        return list;
+    }
+    public static void main(String[] args) {
+        String[] words = new String[]{"cat","cats","catsdogcats","dog","catcatdagcat","dagdogdogcat","catdogcatdogcatdogcatdogcatdog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"};
+        List<String> list = ConcatenatedWords.findAllConcatenatedWordsInADict(words);
+        for(String s : list) {
+            System.out.println(s);
+        }
+    }
+}
